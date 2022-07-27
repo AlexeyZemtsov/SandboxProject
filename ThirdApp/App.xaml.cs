a180ef6fd05b202e0f3f227ebfac394402c8fd02
Alexey Zemtsov <zemtsov_60@mail.ru>
2022-07-27 19:56:56 +0300

    initial commit

---
+++ ThirdApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ThirdApp;
+
+public partial class App : Application
+{
+  public App()
+  {
+    InitializeComponent();
+
+    MainPage = new AppShell();
+  }
+}
---
+++ ThirdApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ThirdApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
