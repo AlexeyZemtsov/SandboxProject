9b87114efee8d64b1f89ce2f6de26b9cdc7d8540
Alexey Zemtsov <zemtsov_60@mail.ru>
2022-07-28 14:15:01 +0300

    Добавил кнопку, которая вызывает карты

--- SandboxApp/MainPage.xaml.cs
+++ SandboxApp/MainPage.xaml.cs
@@ -2,24 +2,32 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+    private async void OnMapsClicked(object sender, EventArgs e)
+    {
+        bool supportsUri = await Launcher.Default.CanOpenAsync("maps://");
+
+        if (supportsUri)
+            await Launcher.Default.OpenAsync("maps://");
+
+    }
 }
 
 
