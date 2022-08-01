1e1c350950493cbf3b0bff4c5e59972b0f2fbce5
Alexey Zemtsov <zemtsov_60@mail.ru>
2022-08-01 17:00:36 +0300

    Переделал интерфейс

--- SandboxApp/MainPage.xaml.cs
+++ SandboxApp/MainPage.xaml.cs
@@ -2,25 +2,30 @@
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
+    string name1 = "Name1";
+    string value1 = "Value1";
+    string name2 = "Name2";
+    string value2 = "Value2";
+    string name3 = "Name3";
+    string value3 = "Value3";
 
     public MainPage()
     {
         InitializeComponent();
 
-        CalcBtn.Text = State.GetState().Field1;
+        //        CalcBtn.Text = State.GetState().Field1;
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        count++;
+        //count++;
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+        //if (count == 1)
+        //    CounterBtn.Text = $"Clicked {count} time";
+        //else
+        //    CounterBtn.Text = $"Clicked {count} times";
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        //SemanticScreenReader.Announce(CounterBtn.Text);
     }
     private async void OnMapsClicked(object sender, EventArgs e)
     {
@@ -33,24 +38,60 @@ public partial class MainPage : ContentPage
 
     private void OnCalcClicked(object sender, EventArgs e)
     {
-        CalcBtn.Text = State.GetState().Field1;
+        OutputLabel.Text = State.GetState().Field1;
+
     }
 
     private async void OnURLClicked(object sender, EventArgs e)
     {
+        //"sandbox-app://ridetype?first=1111&second=2222"
+        string host = "ridetype";
+
         bool supportsUri = await Launcher.Default.CanOpenAsync("sandbox-app://");
 
         if (supportsUri)
-            await Launcher.Default.OpenAsync("sandbox-app://ridetype?first=1111&second=2222");
+        {
+            string urlstring = "sandbox-app://";
+            urlstring = String.Concat(urlstring, host, "?", name1, "=", value1, "&", name2, "=", value2, "&", name3, "=", value3);
+
+            await Launcher.Default.OpenAsync(urlstring);
+        }
         else
-            CalcBtn.Text = $"Нет URL";
+            OutputLabel.Text = $"Нет URL";
 
     }
 
     public void UpdateCalcBtn(string newLabel)
     {
-        CalcBtn.Text = newLabel;
+        OutputLabel.Text = newLabel;
     }
+
+    public void CompletedName1(object sender, EventArgs e)
+    {
+        name1 = ((Entry)sender).Text;
+    }
+    public void CompletedValue1(object sender, EventArgs e)
+    {
+        value1 = ((Entry)sender).Text;
+    }
+    public void CompletedName2(object sender, EventArgs e)
+    {
+        name2 = ((Entry)sender).Text;
+    }
+    public void CompletedValue2(object sender, EventArgs e)
+    {
+        value2 = ((Entry)sender).Text;
+    }
+    public void CompletedName3(object sender, EventArgs e)
+    {
+        name3 = ((Entry)sender).Text;
+    }
+    public void CompletedValue3(object sender, EventArgs e)
+    {
+        value3 = ((Entry)sender).Text;
+    }
+
+
 }
 
 
